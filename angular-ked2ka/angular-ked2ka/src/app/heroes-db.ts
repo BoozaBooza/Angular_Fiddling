66e5ce928a9d2a008cf3037763fd1ce7562b0d02
Dheeraj Butola <dheeraj.butola11@gmail.com>
2019-01-14 17:02:16 +0530

    Add files via upload

---
+++ angular-ked2ka/angular-ked2ka/src/app/heroes-db.ts
@@ -0,0 +1,28 @@
+import { Hero } from './hero';
+
+export const Heroes : Hero[] = [
+{
+  id : 1,
+  name : "One"
+},
+{
+  id : 2,
+  name : "Two"
+},
+{
+  id : 3,
+  name : "Three"
+},
+{
+  id : 4,
+  name : "Four"
+},
+{
+  id : 5,
+  name : "Five"
+},
+{
+  id : 6,
+  name : "Six"
+}
+];
